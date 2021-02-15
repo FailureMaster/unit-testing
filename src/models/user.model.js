c036be6259ba70c0ef01cd87eb1b3d7b24a85a69
Oliver Rosales <oliverjohnpr2013@gmail.com>
2021-02-15 11:57:25 +0800

    Initial commit

---
+++ src/models/user.model.js
@@ -0,0 +1,15 @@
+const Mysql                 = require('mysql');
+const executeQuery  		= require('./config/database.js');
+
+class UserModel {
+
+	async getUsers() {
+
+	}
+
+	async createUser(params){
+
+	}
+}
+
+module.exports = UserModel;
---
+++ src/test/user.test.js
@@ -0,0 +1,20 @@
+const chai                  = require('chai');
+const expect                = chai.expect;
+const Connection            = require('../config/database');
+const UserModel             = require('../models/user.model');
+
+describe("User Model", function(){
+    it("Given a valid input, it should return true if we successfully create a new user", async function(){
+        let userModel   = new UserModel();
+        let params      = {"name": "Oliver Rosales", "email": "jrosales@village88.com", "password": "password123"};
+
+        let result = await userModel.createUser(params);
+
+        expect(result).to.equal(true);
+    });
+
+    after(function(){
+        /* close DB connection */ 
+        Connection.end();
+    });
+});
