c32ee56de262fe911844f200198c50a338d28d1a
FailureMaster <mike.macabulos@gmail.com>
2024-04-02 15:52:33 +0800

    Added a code for insert statement for the test to return true

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -8,7 +8,18 @@ class UserModel {
 	}
 
 	async createUser(params){
-  
+		let result = false;
+		try{
+			let create_user_query  = Mysql.format(`INSERT INTO users(name,email,password) VALUES(?,?,?);`, [params.name, params.email, params.password]);
+			let create_user_result = await executeQuery(create_user_query);
+			if(create_user_result){
+					result = true;
+			}
+		}catch(err){
+			console.log(err);
+		};
+		
+		return result;    
 	}
 }
 
